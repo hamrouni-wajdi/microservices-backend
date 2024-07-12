6c4d45fe9ce585d7e963f9b7c65b3ecd5e35551e
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2024-07-12 10:52:16 +0100

    db sync

--- services/db/index.js
+++ services/db/index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express()
 app.use(express.json());
 require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const sequelize = require('./connection.js'); // Import Sequelize connection
+// Sync Sequelize models with the database
+sequelize.sync()
+  .then(() => {
+    console.log('Database and tables synchronized.');
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
 
 app.get("/",(req,res)=>{
     return res.status(200).json({"msg": "Hello from DB"})

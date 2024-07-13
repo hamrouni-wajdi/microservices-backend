43b531741a7f219865a98f24e2323b653b6c3a3e
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2024-07-14 00:58:53 +0100

    payment+notif

--- services/notification/index.js
+++ services/notification/index.js
@@ -2,6 +2,50 @@ const express = require("express");
 const app = express()
 app.use(express.json());
 require('dotenv').config();
+const webPush = require('web-push');
+const bodyParser = require('body-parser');
+
+// Generate VAPID keys
+const vapidKeys = webPush.generateVAPIDKeys();
+
+console.log(vapidKeys, "vapidkeys");
+webPush.setVapidDetails(
+    'mailto:your-email@example.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+  );
+
+  app.use(bodyParser.json());
+
+  let subscriptions = [];
+ 
+// Endpoint to save subscription objects
+app.post('/subscribe', (req, res) => {
+    const subscription = req.body;
+    subscriptions.push(subscription);
+    res.status(201).json({});
+  });
+  
+  // Endpoint to trigger push notifications
+  app.post('/sendNotification', (req, res) => {
+    const notificationPayload = {
+      notification: {
+        title: 'New Notification',
+        body: 'This is a push notification',
+        icon: 'assets/icons/icon-512x512.png',
+      },
+    };
+    const promises = subscriptions.map(sub => 
+        webPush.sendNotification(sub, JSON.stringify(notificationPayload))
+      );
+    
+      Promise.all(promises)
+        .then(() => res.status(200).json({ message: 'Notifications sent' }))
+        .catch(err => {
+          console.error('Error sending notification:', err);
+          res.sendStatus(500);
+        });
+    });
 
 app.get("/",(req,res)=>{
     return res.status(200).json({"msg": "Hello from notification"})
--- services/payment/index.js
+++ services/payment/index.js
@@ -3,9 +3,27 @@ const app = express()
 app.use(express.json());
 require('dotenv').config();
 const amqplib = require('amqplib/callback_api');
-const queue = 'tasks';
+const bodyParser = require('body-parser');
+const stripe = require('stripe')('YOUR_SECRET_KEY'); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.post('/create-payment-intent', async (req, res) => {
+    const { amount } = req.body;
+  
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+      });
+  
+      res.status(200).send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  });
 
 app.get("/",(req,res)=>{
     return res.status(200).json({"msg": "Hello from Payment"})
@@ -15,34 +33,3 @@ app.listen(process.env.PAYMENT_PORT, () => {
     console.log(`payment service is Listening to Port ${process.env.PAYMENT_PORT}`)
 })
 
-amqplib.connect('amqp://localhost', (err, conn) => {
-    if (err) throw err;
-  
-    // Listener
-//     conn.createChannel((err, ch2) => {
-//       if (err) throw err;
-  
-//       ch2.assertQueue(queue);
-  
-//       ch2.consume(queue, (msg) => {
-//         if (msg !== null) {
-//           console.log(msg.content.toString());
-//           ch2.ack(msg);
-//         } 
-//         else {
-//           console.log('Consumer cancelled by server');
-//         }
-//       }
-//     );
-//     }
-// );
-  
-    // Sender
-    conn.createChannel((err, ch1) => {
-      if (err) throw err;
-  
-      ch1.assertQueue(queue);
-  
-        ch1.sendToQueue(queue, Buffer.from('message from payment'));
-    });
-  });

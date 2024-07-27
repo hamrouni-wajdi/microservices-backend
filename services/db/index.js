8a4f2360fb1cc8d3a583c4d0b1bf56c2d937ccae
hamrouni-wajdi <hamrouniwajdi14@gmail.com>
2024-07-28 00:24:46 +0100

    init

--- services/db/index.js
+++ services/db/index.js
@@ -25,7 +25,7 @@ app.get("/",(req,res)=>{
 
 
 app.post("/register",async(req,res)=>{
-  console.log("register customer ===============================")
+  console.log("register customer ===============================",req.body)
   
   try {
       const { username, email, password, firstname,lastname,address,phone } = req.body;
